eb9eab1fa98179286b758210e98f56bc84a36096
Vog2k <99256921+Vog2k@users.noreply.github.com>
2022-05-17 22:21:57 +1200

    small changes made

--- main.py
+++ main.py
@@ -31,39 +31,42 @@ while True:
     # My attempt at trying to have a small search function,I hoped for the user to type in their name to acquire their
     # name and also see a grade next to their name, but it's proven to get the best of me as of this moment
     if Search == "Nidhi Gowdra" or "N.G" or "Nidhi":
+        print()
         print("Welcome Nidhi Gowdra.")
-        score = int(input('Please enter your score:'))  # This line will appear when user has correct name
-
-        if 90 <= score <= 100:  # Grater than or equal to 90pts or less than or equal to 100pts
+        print()
+        score = int(input('Please enter your IT Systems score:'))  # This line will appear when user has correct name
+        score2 = int(input("Please enter your programming principles score"))
+        final_results = score + score2
+        if 90 <= final_results <= 100:  # Grater than or equal to 90pts or less than or equal to 100pts
             print("Great job you've achieved a grade of")
             print('"A"')
-            print('Please continue')
-            print('If you would like to quit type "Q" or "q"')
-        elif 75 <= score <= 89:  # Grater than or equal to 75pts or less than or equal to 89pts
+            print('Please continue'"\n")
+            print('If you would like to quit type "Q" or "q"'"\n")
+        elif 75 <= final_results <= 89:  # Grater than or equal to 75pts or less than or equal to 89pts
             print("Great job you've achieved a grade of")
             print('"B"')
-            print('Please continue')
-            print('If you would like to quit type "Q" or "q"')
-        elif 60 <= score <= 74:  # Grater than or equal to 60pts or less than or equal to 74pts
+            print('Please continue'"\n")
+            print('If you would like to quit type "Q" or "q"'"\n")
+        elif 60 <= final_results <= 74:  # Grater than or equal to 60pts or less than or equal to 74pts
             print("Great job you've achieved a grade of")
             print('"C"')
-            print('Please continue')
-            print('If you would like to quit type "Q" or "q"')
-        elif 45 <= score <= 59:  # Grater than or equal to 45pts or less than or equal to 59pts
+            print('Please continue'"\n")
+            print('If you would like to quit type "Q" or "q"'"\n")
+        elif 45 <= final_results <= 59:  # Grater than or equal to 45pts or less than or equal to 59pts
             print("Great job you've achieved a grade of")
             print('"D"')
-            print('Please continue')
-            print('If you would like to quit type "Q" or "q"')
-        elif 30 <= score >= 44:  # Grater than or equal to 30pts or less than or equal to 44pts
+            print('Please continue'"\n")
+            print('If you would like to quit type "Q" or "q"'"\n")
+        elif 30 <= final_results >= 44:  # Grater than or equal to 30pts or less than or equal to 44pts
             print("Great job you've achieved a grade of")
             print('"E"')
-            print('Please continue')
-            print('If you would like to quit type "Q" or "q"')
-        elif 0 <= score <= 10:  # Grater than or equal to 0pts or less than or equal to 10pts
+            print('Please continue'"\n")
+            print('If you would like to quit type "Q" or "q"'"\n")
+        elif 0 <= final_results <= 10:  # Grater than or equal to 0pts or less than or equal to 10pts
             print("Great job you've achieved a grade of")
             print('"F"')
-            print('Please continue')
-            print('If you would like to quit type "Q" or "q"')
+            print('Please continue'"\n")
+            print('If you would like to quit type "Q" or "q"'"\n")
     else:  # Else statement was incase the user typed in the wrong name or number in the search area
         print('Failed, Please try again')
     if Search == "Q" or Search == "q":
